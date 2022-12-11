f17d4746d2ae1e603f4b1b6a31989fa820ea2c2e
sfc-gh-sehrlich1 <samuel.ehrlich@snowflake.com>
2022-12-11 15:09:13 -0500

    my commit

--- main.py
+++ main.py
@@ -6,6 +6,29 @@ from snowflake.snowpark.functions import call_udf, col
 from snowflake.snowpark import functions as f
 import json
 
+if not hasattr(st, 'already_started_server'):
+    # Hack the fact that Python modules (like st) only load once to
+    # keep track of whether this file already ran.
+    st.already_started_server = True
+
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
+
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
+
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/foo')
+    def serve_foo():
+        return 'This page is served via Flask!'
+
+    app.run(port=8501)
+
 # Display Content #
 
 with open('style.css') as f:
